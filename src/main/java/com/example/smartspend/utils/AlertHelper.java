2bb6d60d16f73d38b9761110fb4a269078801280
LeThuy <leethuyy06@gmail.com>
2026-04-10 19:04:31 +0700

    feat: Xây dựng form Thêm Giao dịch, kết nối Database và hiển thị UI

--- src/main/java/com/example/smartspend/utils/AlertHelper.java
+++ src/main/java/com/example/smartspend/utils/AlertHelper.java
@@ -1,4 +1,14 @@
 package com.example.smartspend.utils;
 
+import javafx.scene.control.Alert;
+
 public class AlertHelper {
+
+    public static void showAlert(Alert.AlertType type, String title, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 }
